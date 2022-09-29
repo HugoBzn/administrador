8009b651b9b22ef934d704b8860c48d7493aee36
HugoBzn <hugobazan1499@gmail.com>
2022-09-29 02:01:56 -0500

    Navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,10 +1,20 @@
 <template>
-  <div>
-    <!-- Image and text -->
-    <nav class="navbar navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarTogglerDemo01"
+      aria-controls="navbarTogglerDemo01"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
       <a class="navbar-brand" href="#">
         <img
-          src="/docs/4.0/assets/brand/bootstrap-solid.svg"
+          src="@/assets/images/logo.png"
           width="30"
           height="30"
           class="d-inline-block align-top"
@@ -12,8 +22,21 @@
         />
         Bootstrap
       </a>
-    </nav>
-  </div>
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li class="nav-item active">
+          <a class="nav-link" href="#"
+            >Home <span class="sr-only">(current)</span></a
+          >
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
