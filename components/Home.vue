b68b2babbc18d8748127aa1391c1ecad61512fde
HugoBzn <hugobazan1499@gmail.com>
2022-09-29 04:06:20 -0500

    Update Home.vue

--- components/Home.vue
+++ components/Home.vue
@@ -1,10 +1,18 @@
 <template>
   <div style="padding: 2rem">
     <h1>Bienvenido a Yonce-Ce</h1>
-    <br>
+    <br />
     <p>
       La aplicación que reformará la manera en la que LATAM revolucionará y
       evolucionará la monetización actual.
     </p>
+    <br />
+    <center>
+      <img
+        src="@/assets/images/ave_huicahapan-01.svg"
+        width="500"
+        height="400"
+      />
+    </center>
   </div>
 </template>
