7ef07ea6c7350b5d50d672c3021012f48248918a
HugoBzn <hugobazan1499@gmail.com>
2022-09-29 04:19:07 -0500

    About page

---
+++ components/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div style="padding: 3rem">
+    <h1>Hola, somos Yonce-Ce</h1>
+    <br />
+    <p>
+      Somos un equipo por parte del Instituto Tecnológico de la Gustavo A.
+      Madero, orgullosos de estar aquí presentes en el Hackatec 2022.
+    </p>
+    <center>
+      <img src="@/assets/images/itgam.jpg" width="600" />
+    </center>
+    <br />
+    <p>
+      Estamos felices de haber llegado hasta aquí, esperando que todo salga de
+      la mejor manera posible, el estar aquí, ya es ganar!
+    </p>
+    <br />
+    <center><h1>🎉 ¡MUCHAS GRACIAS! 🥳</h1></center>
+  </div>
+</template>
